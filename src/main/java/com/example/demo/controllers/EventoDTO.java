23fbab6645bac73e44d5698d2351639393f59cf5
TXCAndrea <andrea.grigolo@edu.unife.it>
2025-06-26 11:16:07 +0200

    Add files via upload

---
+++ src/main/java/com/example/demo/controllers/EventoDTO.java
@@ -0,0 +1,20 @@
+package com.example.demo.dto;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class EventoDTO {
+    private Long id;
+    private String luogo;
+    private LocalDateTime dataora;
+    private float costo;
+    private int posti;
+    private Long idOrganizzatore;
+    private Long idCategoria;
+}
