6e124ec9cb3d1cb75e881c9dc3b40a76f8d645e4
SuperT457 <giulio.tosi.gt04@gmail.com>
2025-09-04 11:57:28 +0200

    update databse dump

--- provincia_dump.sql
+++ provincia_dump.sql
@@ -54,13 +54,16 @@ CREATE TABLE `evento` (
   `costo` float NOT NULL,
   `n_posti` int DEFAULT NULL,
   `organizzatore` bigint NOT NULL,
-  `categoria` int NOT NULL,
+  `categoria` int DEFAULT NULL,
+  `titolo` varchar(45) DEFAULT NULL,
+  `descrizione` text,
+  `image_url` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_categoria_idx` (`categoria`),
   KEY `fk_organizzatore_idx` (`organizzatore`),
-  CONSTRAINT `fk_categoria` FOREIGN KEY (`categoria`) REFERENCES `categoria` (`id`),
-  CONSTRAINT `fk_organizzatore` FOREIGN KEY (`organizzatore`) REFERENCES `utente` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  CONSTRAINT `fk_organizzatore` FOREIGN KEY (`organizzatore`) REFERENCES `utente` (`id`),
+  CONSTRAINT `FKs9mxy9b6nw8g6uj8x2pd95euk` FOREIGN KEY (`categoria`) REFERENCES `categoria` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -69,8 +72,33 @@ CREATE TABLE `evento` (
 
 LOCK TABLES `evento` WRITE;
 /*!40000 ALTER TABLE `evento` DISABLE KEYS */;
-INSERT INTO `evento` VALUES (9,'Milano','2025-08-10 21:00:00',15,100,1,17),(10,'Roma','2025-09-05 18:30:00',0,200,1,18),(11,'Torino','2025-08-15 20:00:00',25,80,1,19),(12,'Bologna','2025-09-20 17:00:00',10,150,1,20),(13,'Napoli','2025-10-01 19:00:00',12.5,120,1,21),(14,'Firenze','2025-08-30 10:00:00',5,300,1,22),(15,'Verona','2025-08-12 22:00:00',8,50,1,23),(16,'Genova','2025-09-10 11:00:00',0,500,1,24),(17,'Trento','2025-09-18 14:00:00',0,100,1,25),(18,'Padova','2025-10-05 16:00:00',2,80,1,26),(19,'Pavia','2025-10-12 15:30:00',0,60,1,27),(20,'Lecce','2025-09-01 19:30:00',7,90,1,28),(21,'Ferrara','2025-08-25 20:00:00',6,150,1,29),(22,'Reggio Emilia','2025-09-22 09:00:00',0,300,1,30),(23,'Assisi','2025-10-15 18:00:00',3,120,1,31);
+INSERT INTO `evento` VALUES (9,'Milano','2025-08-10 21:00:00',15,100,1,17,'prova!','aaaa','/images/bello-figo.webp'),(10,'Roma','2025-09-05 18:30:00',0,200,1,18,'prova!','aaaa','/images/bello-figo.webp'),(11,'Torino','2025-08-15 20:00:00',25,80,1,19,'prova!','aaaa','/images/bello-figo.webp'),(12,'Bologna','2025-09-20 17:00:00',10,150,1,20,'prova!','aaaa','/images/bello-figo.webp'),(13,'Napoli','2025-10-01 19:00:00',12.5,120,1,21,'prova!','aaaa','/images/bello-figo.webp');
 /*!40000 ALTER TABLE `evento` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `organizzatore`
+--
+
+DROP TABLE IF EXISTS `organizzatore`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `organizzatore` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `organizzatore`
+--
+
+LOCK TABLES `organizzatore` WRITE;
+/*!40000 ALTER TABLE `organizzatore` DISABLE KEYS */;
+/*!40000 ALTER TABLE `organizzatore` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -81,7 +109,7 @@ DROP TABLE IF EXISTS `prenotazione`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `prenotazione` (
-  `id` bigint NOT NULL,
+  `id` bigint NOT NULL AUTO_INCREMENT,
   `utente_id` bigint NOT NULL,
   `evento_id` bigint NOT NULL,
   `dataora` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -90,7 +118,7 @@ CREATE TABLE `prenotazione` (
   KEY `fk_utente_partecipante_idx` (`utente_id`),
   CONSTRAINT `fk_evento` FOREIGN KEY (`evento_id`) REFERENCES `evento` (`id`),
   CONSTRAINT `fk_utente_partecipante` FOREIGN KEY (`utente_id`) REFERENCES `utente` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -99,6 +127,7 @@ CREATE TABLE `prenotazione` (
 
 LOCK TABLES `prenotazione` WRITE;
 /*!40000 ALTER TABLE `prenotazione` DISABLE KEYS */;
+INSERT INTO `prenotazione` VALUES (1,1,9,'2025-08-29 11:11:39'),(2,1,9,'2025-08-29 11:43:21'),(3,2,9,'2025-08-29 11:44:05');
 /*!40000 ALTER TABLE `prenotazione` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -112,14 +141,13 @@ DROP TABLE IF EXISTS `utente`;
 CREATE TABLE `utente` (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `email` varchar(45) NOT NULL,
-  `username` varchar(45) NOT NULL,
+  `username` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
   `password` varchar(45) NOT NULL,
-  `telefono` varchar(45) DEFAULT NULL,
   `ruolo` enum('u','o') NOT NULL DEFAULT 'u',
   PRIMARY KEY (`id`),
   UNIQUE KEY `email_UNIQUE` (`email`),
   UNIQUE KEY `username_UNIQUE` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -128,7 +156,7 @@ CREATE TABLE `utente` (
 
 LOCK TABLES `utente` WRITE;
 /*!40000 ALTER TABLE `utente` DISABLE KEYS */;
-INSERT INTO `utente` VALUES (1,'cicciogamer@gmail.com','ciccio','paguro','','o');
+INSERT INTO `utente` VALUES (1,'cicciogamer@gmail.com','ciccio','paguro','o'),(2,'supert@email.com','SuperT','SuperT457','u'),(4,'supert2@email.com','SuperT2','SuperT457','u'),(5,'email@email.com','username','password123','u'),(6,'electronic@mail','impazzisco','password','u');
 /*!40000 ALTER TABLE `utente` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -141,4 +169,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-07-30  9:22:11
+-- Dump completed on 2025-09-04 11:57:14
