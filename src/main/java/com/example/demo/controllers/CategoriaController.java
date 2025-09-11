b24c6cd4d899e02b229655f857ff8131816dd499
SuperT457 <giulio.tosi.gt04@gmail.com>
2025-09-11 10:40:47 +0200

    fix cross origin

--- src/main/java/com/example/demo/controllers/CategoriaController.java
+++ src/main/java/com/example/demo/controllers/CategoriaController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controllers;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +11,7 @@ import com.example.demo.services.CategoriaService;
 
 @RestController
 @RequestMapping("/api/categorie")
+@CrossOrigin(origins = "http://localhost:4200")
 public class CategoriaController {
     @Autowired
     private CategoriaService categoriaService;
